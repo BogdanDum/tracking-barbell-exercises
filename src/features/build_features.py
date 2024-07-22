60d8749c7b52ccd9e1ac8b7ff03a196869170ca7
bogdanduminica <B.S.Duminica@student.tudelft.nl>
2024-07-22 21:46:54 +0300

    Decided upon the most optimal number of clusters (5) by using the elbow technique

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 from TemporalAbstraction import NumericalAbstraction
 from FrequencyAbstraction import FourierTransformation
-from sklearn.cluster import k_means
+from sklearn.cluster import KMeans
 
 # Load data
 df = pd.read_pickle("../../data/interim/removed_outliers_chauvenet_02.pkl")
@@ -215,3 +215,51 @@ df_freq = df_freq.iloc[::2]
 
 # Clustering 
 
+df_cluster = df_freq.copy()
+
+cluster_columns = ["acc_x", "acc_y", "acc_z"]
+k_values = range(2, 10)
+inertias = []
+
+for k in k_values:
+    subset = df_cluster[cluster_columns]
+    kmeans = KMeans(n_clusters = k, n_init = 20, random_state = 0)
+    cluster_labels = kmeans.fit_predict(subset)
+    inertias.append(kmeans.inertia_)
+
+plt.figure(figsize=(10, 10))
+plt.plot(k_values, inertias)
+plt.xlabel("k")
+plt.ylabel("Sum of squared distances")
+plt.show()
+
+# 4 or 5?
+# 5 seems to be the most optimal number of clusters
+
+kmeans = KMeans(n_clusters=5, n_init=20, random_state=0)
+subset = df_cluster[cluster_columns]
+df_cluster["cluster"] = kmeans.fit_predict(subset)
+
+# visualize to see if the clusters make sense
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection = "3d")
+for c in df_cluster["cluster"].unique():
+    subset = df_cluster[df_cluster["cluster"] == c]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+ax.set_xlabel("x-axis")
+ax.set_ylabel("y-axis")
+ax.set_zlabel("z-axis")
+plt.legend()
+plt.show()
+
+# now split by label (exercise) instead of cluster and compare
+fig = plt.figure(figsize=(15, 15))
+ax = fig.add_subplot(projection = "3d")
+for l in df_cluster["label"].unique():
+    subset = df_cluster[df_cluster["label"] == l]
+    ax.scatter(subset["acc_x"], subset["acc_y"], subset["acc_z"], label=c)
+ax.set_xlabel("x-axis")
+ax.set_ylabel("y-axis")
+ax.set_zlabel("z-axis")
+plt.legend()
+plt.show()
