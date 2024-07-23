695ba912f2463054e4dd5f21e33ca8eedacd5faf
bogdanduminica <B.S.Duminica@student.tudelft.nl>
2024-07-23 14:35:43 +0300

    Imported the necessary libraries and adjusted the necessary plot settings

---
+++ models/train_model.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from LearningAlgorithms import ClassificationAlgorithms
+import seaborn as sns
+import itertools
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
