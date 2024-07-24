782b0d0185c1a3691a5a20470007a380fb1f2300
bogdanduminica <B.S.Duminica@student.tudelft.nl>
2024-07-24 10:15:32 +0300

    Imports and plot settings

---
+++ src/features/count_reps.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from DataTransformation import LowPassFilter
+from scipy.signal import argrelextrema
+from sklearn.metrics import mean_absolute_error
+
+pd.options.mode.chained_assignment = None
+
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
